09747ff6883457c2fbd965c7e5a7a1f9c4231e95
okuya0211 <v.soccer-dog5.k-11.v@docomo.ne.jp>
2018-09-07 14:19:37 +0900

    modify zombie

--- Zombie.java
+++ Zombie.java
@@ -2,7 +2,6 @@
 public class Zombie extends Monster implements Human{
 
 	//パラメータ
-	String name;
 	int at;
 
 	//逃げる
@@ -21,17 +20,14 @@ public class Zombie extends Monster implements Human{
 	}
 
 	//コンストラクタ
-	public Zombie(String name){
+	public Zombie(){
 		this.hp = 30;
-		this.name = name;
 		this.at = 10;
 	}
 
 	//getter,setter
 	public int getHp(){return hp;}
 	public int setHp(int hp){this.hp = hp;}
-	public String getName(){return name}
-	public String setName(String name){this.name = name;}
 	public int getAt(){return at;}
 	public int setAt(int at){this.at = at;}
 }
