34d0b079386f0fd4f134048c147c39493299b902
yu <h12yuuki4@gmail.com>
2018-09-07 13:25:19 +0900

    modify Life

--- Life.java
+++ Life.java
@@ -1,8 +1,16 @@
 abstract class Life {
-	
+
 	private int hp;
 
-	public dead() {
+	public int getHp(){
+		return this.hp;
+	}
 
+	public void setHp(int hp){
+		this.hp = hp;
+	}
+	
+	public dead() {
+		
 	}
 }
