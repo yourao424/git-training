f39175218f4dd558fddd18542ba1900b7cf222bf
Yuhei FUJITA <fujita@devinker.com>
2018-09-07 11:48:37 +0900

    Created Yatagarasu.java

---
+++ Yatagarasu.java
@@ -0,0 +1,20 @@
+class Yatagarasu extends Monster implements Briad {
+    
+    Yatagarasu(int hp) {
+        this.hp = hp;
+    }
+
+    public void attack() {
+        System.out.println("八咫烏の攻撃");
+        return;
+    }
+
+    public dead() {
+        System.out.println("八咫烏は死んだ");
+    }
+
+    public void run() {
+        System.out.print("八咫烏は走った！");
+        return;
+    }
+}
