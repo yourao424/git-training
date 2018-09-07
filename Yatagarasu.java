731c922d9ac23ced4b208f5388d0ba2d675746a5
Yuhei FUJITA <fujita@devinker.com>
2018-09-07 13:15:01 +0900

    コメントの付加

--- Yatagarasu.java
+++ Yatagarasu.java
@@ -1,9 +1,17 @@
 public class Yatagarasu extends Monster implements Briad {
     
+    /**
+     * コンストラクタ
+     * @param hp ヤタガラスのHP
+     */
     Yatagarasu(int hp) {
         this.hp = hp;
     }
 
+    /**
+     * ヤタガラスの攻撃
+     * @param monster 攻撃対象のインスタンス
+     */
     public void attack(Monster monster) {
         System.out.println("ヤタガラスの攻撃");
         monster.setHp(monster.getHp() - 100);
@@ -11,10 +19,16 @@ public class Yatagarasu extends Monster implements Briad {
         return;
     }
 
+    /**
+     * ヤタガラスの死
+     */
     public void dead() {
         System.out.println("ヤタガラスは死んだ");
     }
 
+    /**
+     * ヤタガラスの疾走
+     */
     public void run() {
         System.out.print("ヤタガラスは走った！");
         return;
