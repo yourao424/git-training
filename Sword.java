ff777ed9c53226ca477a117ae15aafaa218664d5
yu <h12yuuki4@gmail.com>
2018-09-10 13:56:19 +0900

    added sword

---
+++ Sword.java
@@ -0,0 +1,20 @@
+public class Sword extends Weapon {
+
+	Sword(String name, int power){
+		this.setName(name);
+		this.setPower(power);
+	}
+
+	public void attack(Character character, Monster monster) {
+		System.out.println(character.getName() + "は" + this.getName() + "で攻撃");
+		monster.setHp(monster.getHp() -500);
+		System.out.println(monster.getName() + "に" + this.getPower() + "のダメージ");
+	}
+
+	public void Throw(Character character, Monster monster) {
+		System.out.println(character.getName() + "は" + this.getName() + "を投げた");
+		monster.setHp(monster.getHp() -750);
+		System.out.println(monster.getName() + "に" + this.getPower() + "のダメージ");
+
+	}
+}
