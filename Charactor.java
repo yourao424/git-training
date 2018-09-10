ff82afaf73148747e44689474e2cc7c52aae4968
mori <ryouhei.m101@gmail.com>
2018-09-10 13:23:16 +0900

    character

--- Charactor.java
+++ Charactor.java
@@ -1,35 +1,36 @@
 public abstract class Charactor extends Life{
 
-private int lv;
-private String name;
-private Weapon weapon;
+	private int lv;
+	private String name;
+	private Weapon weapon;
 
-//Attackメソッド
-abstract void Attack();
 
-//lvのgetterとsetter
-public int getLv(){
-	return this.lv;
-}
-public void setLv(int lv){
-	this.lv = lv;
-}
+	//Attackメソッド
+	abstract void attack(Life life);
 
-//nameのgetterとsetter
-public String getName(){
-	return this.name;
-}
-public void setName(String name){
-	this.name = name;
-}
+	//lvのgetterとsetter
+	public int getLv(){
+		return this.lv;
+	}
+	public void setLv(int lv){
+		this.lv = lv;
+	}
 
-//weaponのgetterとsetter
-public Weapon getWeapon(){
-	return this.weapon;
-}
+	//nameのgetterとsetter
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
 
-public void setWeapon(Weapon weapon){
-	this.weapon = weapon;
-}
+	//weaponのgetterとsetter
+	public Weapon getWeapon(){
+		return this.weapon;
+	}
+
+	public void setWeapon(Weapon weapon){
+		this.weapon = weapon;
+	}
 
 }
