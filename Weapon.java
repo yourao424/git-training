8e114d6211d91ed4416db2252b649c69a4015d21
yourao424 <yourao424@gmail.com>
2018-09-10 13:25:43 +0900

    Weapon.java Wand.java created

---
+++ Weapon.java
@@ -0,0 +1,23 @@
+public abstract class Weapon {
+	private String name;
+	private int power;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public void Throw(Monster monster) {
+
+	}
+	public void Attack(Monster monster) {
+
+	}
+}
