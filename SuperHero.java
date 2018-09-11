74174ecd38bfcd2059ad1457c47affc722779d13
yourao424 <yourao424@gmail.com>
2018-09-11 10:35:38 +0900

    SuperHero attack method fixed

--- SuperHero.java
+++ SuperHero.java
@@ -18,17 +18,7 @@ public class SuperHero extends Hero{
 	}
 
 	public void attack(Life monster) {
-		if(super.getWeapon() == null){
-			System.out.println("スーパー勇者" + super.getName() + "のパンチ");
-			super.setHp(super.getHp() - 10);
-			System.out.println("スーパー勇者" + super.getName() + "のパンチ");
-			super.setHp(super.getHp() - 10);
-		}else{
-			System.out.println("スーパー勇者" + super.getName() + "の攻撃");
-			super.getWeapon().attack(this, monster);
-			System.out.println("スーパー勇者" + super.getName() + "の攻撃");
-			super.getWeapon().attack(this, monster);
-
-		}
+		super.attack(monster);
+		super.attack(monster);
 	}
 }
