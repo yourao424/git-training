2c346086ceb3e813ab2e65d4f64da3d81240ccbf
yourao424 <yourao424@gmail.com>
2018-09-11 10:12:49 +0900

    modified Monster.java create SuperHero.java

--- Monster.java
+++ Monster.java
@@ -1,6 +1,8 @@
 abstract class Monster extends Life{
 
-	private String name;
+	private String name;	//複数体の判別のため
+	private int exp;	//モンスターごとに経験値を割り振る
+
 	abstract void attack(Life monster);
 
 	public String getName(){
@@ -9,5 +11,13 @@ abstract class Monster extends Life{
 
 	public void setName(String name){
 		this.name = name;
+	}
+
+	public int getExp() {
+		return exp;
+	}
+
+	public void setExp(int exp) {
+		this.exp = exp;
 	}
 }
