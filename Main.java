c216986f3d1f4876124129fe18780b4ec9cea0d1
mori <ryouhei.m101@gmail.com>
2018-09-11 13:20:34 +0900

    Wizard更新

--- Main.java
+++ Main.java
@@ -2,20 +2,24 @@ public class Main{
 	public static void main(String[] args){
 		Yatagarasu yataA = new Yatagarasu(100,"ヤタガラスA");
 		Yatagarasu yataB = new Yatagarasu(100,"ヤタガラスB");
-		Zombie zomA = new Zombie(9999,"ゾンビA");
-		Hero fujii = new Hero("藤井",99,3000);
-		Wizard ito = new Wizard("伊藤",99,500);
+		Zombie zomA = new Zombie(100,"ゾンビA");
+		Hero fujii = new Hero("藤井",1,100);
+		Wizard ito = new Wizard("伊藤",1,100);
+		SuperHero hujiawara = new SuperHero("藤原");
 		Sword sword = new Sword("口トの検");
 		Hagetaka hageA = new Hagetaka();
 		Wand wand = new Wand();
 
 		fujii.setWeapon(sword);
-		System.out.println("藤井は"+sword.getName()+"を装備した。");
-
+		System.out.println(fujii.getName()+"は"+sword.getName()+"を装備した。");
 		do{
 			fujii.attack(yataA);
-			yataB.attack(ito);
-		}while(ito.getHp() >= 0);
-		ito.dead();
+			ito.attack(zomA);
+		}while(zomA.getHp() >= 0);
+		if(zomA.isDead() == true){
+			zomA.dead();
+		}
+
+
 	}
 }
